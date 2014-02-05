76d340fc9898afd70ebbaa22c613a7d16fc0e2e9
kuhar <kubakuderski@gmail.com>
2014-02-05 19:15:43 +0100

    return type fixes for kmu::explode, removal of excessive qualifier

--- explode.h
+++ explode.h
@@ -5,6 +5,11 @@
 *      Author: Jakub 'kuhar' Kuderski
 */
 
+/* INFO:
+*  kmu::explode anables you to call every non-const callable object,
+*  not taking parametrs by rvalue references, with a std::tuple of parameters
+*/
+
 #pragma once
 
 #include <tuple>
@@ -69,30 +74,79 @@ namespace kmu
 		}
 	};
 
-	template <typename Functor, typename... Args>
-	inline auto explode( Functor&& functor, const std::tuple<Args...>& tupleArgs )
-		-> typename impl::x_result_of<Functor( const Args&... )>::type
+	template <int... Index>
+	struct FunctorCaller<void, impl::x_index_tuple_type<Index...>>
 	{
-		return FunctorCaller<impl::x_result_of<Functor( const Args&... )>::type,
-			impl::x_index_tuple<sizeof...( Args )>>
+		template <typename FunctorType, typename TupleType>
+		static inline void call( FunctorType&& functor, TupleType&& tupleArgs )
+		{
+			functor( std::forward<decltype( std::get<Index>(
+				std::forward<TupleType>( tupleArgs ) ) )>
+				( std::get<Index>( std::forward<TupleType>( tupleArgs ) ) )... );
+		}
+
+		template <typename FunctorType, typename TupleType>
+		static inline void call( FunctorType&& functor, const TupleType& tupleArgs )
+		{
+			functor( std::get<Index>( tupleArgs )... );
+		}
+	};
+
+	template <typename Functor, typename... Args,
+		typename ReturnType = impl::x_result_of<Functor( Args... )>::type>
+	inline typename std::enable_if<std::is_same<ReturnType, void>::value, void>::type
+		explode( Functor&& functor, const std::tuple<Args...>& tupleArgs )
+	{
+			FunctorCaller<ReturnType, impl::x_index_tuple<sizeof...( Args )>>
+				::call( std::forward<Functor>( functor ), tupleArgs );
+	}
+
+	template <typename Functor, typename... Args,
+		typename ReturnType = impl::x_result_of<Functor( Args... )>::type>
+	inline typename std::enable_if<!std::is_same<ReturnType,
+		void>::value, ReturnType>::type
+		explode( Functor&& functor, const std::tuple<Args...>& tupleArgs )
+	{
+		return FunctorCaller<ReturnType, impl::x_index_tuple<sizeof...( Args )>>
 			::call( std::forward<Functor>( functor ), tupleArgs );
 	}
 
-	template <typename Functor, typename... Args>
-	inline auto explode( Functor&& functor, std::tuple<Args...>& tupleArgs )
-		-> typename impl::x_result_of<Functor( Args&... )>::type
+	template <typename Functor, typename... Args, 
+		typename ReturnType = impl::x_result_of<Functor( Args... )>::type>
+	inline typename std::enable_if<std::is_same<ReturnType, void>::value, void>::type
+		explode( Functor&& functor, std::tuple<Args...>& tupleArgs )
 	{
-		return FunctorCaller<impl::x_result_of<Functor( Args&... )>::type,
-			impl::x_index_tuple<sizeof...( Args )>>
+		FunctorCaller<ReturnType, impl::x_index_tuple<sizeof...( Args )>>
 			::call( std::forward<Functor>( functor ), tupleArgs );
 	}
 
-	template <typename Functor, typename... Args>
-	inline auto explode( Functor&& functor, std::tuple<Args...>&& tupleArgs )
-		-> typename impl::x_result_of<Functor( Args&&... )>::type
+	template <typename Functor, typename... Args,
+		typename ReturnType = impl::x_result_of<Functor( Args... )>::type>
+	inline typename std::enable_if<!std::is_same<ReturnType,
+		void>::value, ReturnType>::type
+		explode( Functor&& functor, std::tuple<Args...>& tupleArgs )
 	{
-		return FunctorCaller<impl::x_result_of<Functor( Args&&... )>::type,
-			impl::x_index_tuple<sizeof...( Args )>>
+		return FunctorCaller<ReturnType, impl::x_index_tuple<sizeof...( Args )>>
+			::call( std::forward<Functor>( functor ), tupleArgs );
+	}
+
+	template <typename Functor, typename... Args,
+		typename ReturnType = impl::x_result_of<Functor( Args... )>::type>
+	inline typename std::enable_if<std::is_same<ReturnType, void>::value, void>::type
+		explode( Functor&& functor, std::tuple<Args...>&& tupleArgs )
+	{
+		FunctorCaller<ReturnType, impl::x_index_tuple<sizeof...( Args )>>
+			::call( std::forward<Functor>( functor ),
+			std::forward<decltype( tupleArgs )>( tupleArgs ) );
+	}
+
+	template <typename Functor, typename... Args,
+		typename ReturnType = impl::x_result_of<Functor( Args... )>::type>
+	inline typename std::enable_if<!std::is_same<ReturnType,
+		void>::value, ReturnType>::type
+		explode( Functor&& functor, std::tuple<Args...>&& tupleArgs )
+	{
+		return FunctorCaller<ReturnType, impl::x_index_tuple<sizeof...( Args )>>
 			::call( std::forward<Functor>( functor ),
 			std::forward<decltype( tupleArgs )>( tupleArgs ) );
 	}
