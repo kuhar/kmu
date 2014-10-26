a408545e3a139941b2ccfdd08dff232f944fb28c
Jakub Kuderski <kubakuderski@gmail.com>
2014-10-26 14:34:51 +0100

    Coding style updated - function traits updated

--- function_traits.h
+++ function_traits.h
@@ -17,46 +17,46 @@ namespace kmu
 	struct function_traits;
 
 	template<typename ReturnType, typename... Args>
-	struct function_traits<ReturnType( Args... )>
+	struct function_traits<ReturnType(Args...)>
 	{
 		using return_type = ReturnType;
 
-		static const size_t	arity = sizeof... ( Args );
+		static const size_t	arity = sizeof... (Args);
 
 		template <std::size_t N>
 		struct argument
 		{
-			static_assert ( N < arity,
-							"Given index is greater than the number of arguments" );
+			static_assert(N < arity,
+							"Given index is greater than the number of arguments");
 			using type = typename std::tuple_element<N, std::tuple<Args...>>::type;
 		};
 	};
 
 	template<typename ReturnType, typename... Args>
-	struct function_traits<ReturnType(*)( Args... )>
-		: function_traits<ReturnType( Args... )> {};
+	struct function_traits<ReturnType(*)(Args...)>
+		: function_traits<ReturnType(Args...)> {};
 
 	template<typename ReturnType, typename Class, typename... Args>
-	struct function_traits<ReturnType( Class::* )( Args... )>
-		: function_traits<ReturnType( Class*, Args... )> {};
+	struct function_traits<ReturnType(Class::*)(Args...)>
+		: function_traits<ReturnType(Class*, Args...)> {};
 
 	template<typename ReturnType, typename Class, typename... Args>
-	struct function_traits<ReturnType( Class::* )( Args... ) const>
-		: function_traits<ReturnType( const Class*, Args... )> {};
+	struct function_traits<ReturnType(Class::*)(Args...) const>
+		: function_traits<ReturnType(const Class*, Args...)> {};
 
 	template<typename ReturnType, typename Class, typename... Args>
-	struct function_traits<ReturnType( Class::* )( Args... ) volatile>
-		: function_traits<ReturnType( volatile Class*, Args... )> {};
+	struct function_traits<ReturnType(Class::*)(Args...) volatile>
+		: function_traits<ReturnType(volatile Class*, Args...)> {};
 
 	template<typename ReturnType, typename Class, typename... Args>
-	struct function_traits<ReturnType( Class::* )( Args... ) const volatile>
-		: function_traits<ReturnType( const volatile Class*, Args... )> {};
+	struct function_traits<ReturnType(Class::*)(Args...) const volatile>
+		: function_traits<ReturnType(const volatile Class*, Args...)> {};
 
 	template<typename Functor>
 	struct function_traits
 	{
 	private:
-		using x_traits = function_traits<decltype( &Functor::operator() )>;
+		using x_traits = function_traits<decltype(&Functor::operator())>;
 
 	public:
 		using return_type = typename x_traits::return_type;
